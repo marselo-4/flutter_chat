1359b1f353865d2c956c0835f395e840c9165f9b
marselo-4 <carlosmartinezguixa@gmail.com>
2024-05-06 10:17:59 +0200

    cloud firestore temp

--- lib/src/screens/chat_screen.dart
+++ lib/src/screens/chat_screen.dart
@@ -1,6 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_chat/src/services/authentication.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -9,6 +16,25 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final auth = FirebaseAuth.instance;
+  final _fireStore = FirebaseFirestore.instance;
+  TextEditingController messageController = TextEditingController();
+
+  InputDecoration _messageTextFieldDecoration = InputDecoration(
+    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    hintText: 'Type your message here...',
+    border: InputBorder.none,
+  );
+
+  BoxDecoration _messageContainerDecoration = BoxDecoration(
+    border: Border(
+      top: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+    ),
+  );
+
+  TextStyle _sendButtonStyle = TextStyle(
+      color: Colors.lightBlueAccent,
+      fontWeight: FontWeight.bold,
+      fontSize: 18.0);
 
   User? loggedInUser;
   @override
@@ -43,6 +69,35 @@ class _ChatScreenState extends State<ChatScreen> {
             },
           )
         ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: _messageContainerDecoration,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: TextField(
+                    decoration: _messageTextFieldDecoration,
+                    controller: messageController,
+                  )),
+                  ElevatedButton(
+                      onPressed: () {
+                        _fireStore.collection("messages").add({
+                          'value': messageController.text,
+                          'sender': loggedInUser?.email
+                        });
+                      },
+                      child: Text(
+                        "Enviar",
+                        style: _sendButtonStyle,
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
